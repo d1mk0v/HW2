b6765e26df1ceccfe45355e78b0a2531e167d630
d1mk0v <79874763588@ya.ru>
2023-04-19 17:10:00 +0500

    Задача 1

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+        task2();
+    }
+
+    public static void task1 () {
+        System.out.println("Задача 1");
+        var dog = 8.0;
+        System.out.println (dog);
+        var cat = 3.6;
+        System.out.println(cat);
+        var paper = 763789;
+        System.out.println(paper);
+    }
+
+    public static void task2 () {
+        System.out.println("Задача 2");
+       // ... // Пишем код для задачи 2
+    }
+}
